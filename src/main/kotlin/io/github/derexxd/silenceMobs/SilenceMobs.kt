2c7ec6564c1d61bd9e5a270d63954683b0e464f0
Derek <derex1987@gmail.com>
2026-07-04 02:41:54 -0500

    last night at wampus woa jk

---
+++ src/main/kotlin/io/github/derexxd/silenceMobs/SilenceMobs.kt
@@ -0,0 +1,14 @@
+package io.github.derexxd.silenceMobs
+
+import org.bukkit.plugin.java.JavaPlugin
+
+class SilenceMobs : JavaPlugin() {
+
+    override fun onEnable() {
+        // Plugin startup logic
+    }
+
+    override fun onDisable() {
+        // Plugin shutdown logic
+    }
+}
